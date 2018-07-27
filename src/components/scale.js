c6e7dc872e50d7f2eac8021ca6d0f63bbf760ec9
Jesus rey <diegocl02@gmail.com>
2018-07-27 00:26:41 -0500

    created methods in scale.js

--- src/components/scale.js
+++ src/components/scale.js
@@ -4,29 +4,84 @@ import Tone from 'tone'
 export class Scale extends Component {
 
     notes = ["C4", "D4", "E4", "G4", "A4"]
+    newNotes = []
 
     constructor(props){
         super(props)
         this.state = {
-
+            note: null,
+            recordedNotes: []
         }
     }
 
-    playSound(){
+    generateRandomSeq(nSeq) {
+        let newSeq = []
+        for (let i = 0; i < nSeq; i++) {
+            let ranIndex = Math.floor(Math.random() * 5)
+            newSeq.push([this.notes[ranIndex], ranIndex])
+        }
+        return newSeq
+    }
+
+    playInitialNotes(){
         let synth = new Tone.Synth().toMaster()
         let time = '8n'
-
+        console.log(this.newNotes)
+        
         var melody = new Tone.Sequence((time, note) => {
             synth.triggerAttackRelease(note, '4n', time);
-          }, this.notes).start();
+          }, this.newNotes).start();
 
-        melody.start("1m").stop("2m");
-        Tone.Transport.start();
+        melody.start(0).stop("1:1");
+        melody.loop = 1
+        Tone.Transport.bpm.value = 60
+        Tone.Transport.start(1);
+    }
+
+    playNote(n){
+        let synth = new Tone.Synth().toMaster()
+        synth.triggerAttackRelease(this.notes[n], "8n");
+
+        let newRecordedNotes = [...this.state.recordedNotes]
+        newRecordedNotes.push(n)
+        if (newRecordedNotes.length === 5) {
+            this.props.reportRecordedSeq(newRecordedNotes)
+            this.setState({
+                recordedNotes : []
+            })
+        } else if (newRecordedNotes.length < 5) {
+            this.setState({
+                recordedNotes : newRecordedNotes
+            })
+        }
+    }
+
+    componentDidMount(){
+        let newSeq = this.generateRandomSeq(5)
+        this.newNotes = newSeq.map((el) => {return el[0]})
+
+        if(this.props.isPlayable){
+            this.playInitialNotes()
+            let i = 0
+
+            this.props.reportInitialSeq(newSeq.map(el => el[1]))
+            setInterval(()=> {
+                if (i < 5) {
+                this.setState({
+                    note: this.newNotes[i][0]
+                })
+                i++;
+            }
+            }, 1000)
+        }
     }
 
     render(){
-        return (
-            <div onClick={() => {this.playSound()}}> Hello </div>
+        console.log("this state", this.state)
+        return ([
+            <div onClick={() => {}}> <h1>{this.state.note} </h1></div>,
+            <div onClick={() => {this.playNote(2)}}> <h1> play note! </h1></div>
+        ]
         )
     }
 }
