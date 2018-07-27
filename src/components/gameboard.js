edb108fbd37b342b5c7cdea5015c85e9f3c3f81b
Jesus <jesus.reyponce@gmail.com>
2018-07-26 22:41:37 -0500

    Component Gameboard

---
+++ src/components/gameboard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../styles.css';
+
+export class Gameboard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isWinner: 0,
+      step: "Begin",
+      fixed_seq: [],
+      record_seq: [],
+    }
+  }
+  element = []
+
+  setSteps() {
+    switch (this.state.step) {
+      case "Begin":
+        this.setState({step: "Listen"})
+        // element = <Scale />
+        break;
+      case "Listen":
+        this.setState({step: "Scale Play"})
+        break;
+      case "Scale Play":
+        this.setState({step: "Scale Record"})
+        break;
+      case "Scale Record":
+        let seq_equal = this.state.fixed_seq.join() === this.state.record_seq.join()
+        this.setState({step: seq_equal ? "You Win" : "You Lose",
+                      isWinner: seq_equal ? 1 : 0,
+                      })
+        break;
+      default:
+        this.setState({step: "Begin"})
+      }
+  }
+
+  render() {
+    console.log(this.state.isWinner)
+    return (
+      <div className="gameboard">
+        <div className="announcer" onClick={() => this.setSteps()}>
+          {this.state.step}
+        </div>
+        <div>
+          {this.element}
+        </div>
+      </div>
+    );
+  }
+}
